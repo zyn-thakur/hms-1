0133e63d68075bd8ebafc4cfc6b1d0d69e7ea5e1
aladin <kachalshail@gmail.com>
2020-01-02 11:13:20 +0545

    corrected

--- apilogin/services/views.py
+++ apilogin/services/views.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets
 from . import serializers
 from .models import Services
 from rest_framework import response
-# from rest_framework.decorators import action
-# from rest_framework import parsers
+from rest_framework.decorators import action
+from rest_framework import parsers
 
 # Create your views here.
 class ServicesViewset(viewsets.ModelViewSet):
