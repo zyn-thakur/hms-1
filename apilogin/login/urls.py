d3fc934661bbcabf8c4b1288a40dbb0989b56a52
aladin <kachalshail@gmail.com>
2020-01-02 17:10:08 +0545

    zyn changes

--- apilogin/login/urls.py
+++ apilogin/login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from myapi.core import views
-
+from . import  views
 urlpatterns = [
-    # path('hello/', views.HelloView.as_view(), name='hello'),
+    path('', views.show, name='show'),
 ]
--- apilogin/login/views.py
+++ apilogin/login/views.py
@@ -1,11 +1,10 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+# from product.models import Product
 
+def show(request):
+   
+    return render(request,"index.html")
+    #return HttpResponse("welcome to show")
 
-# class HelloView(APIView):
-#     permission_classes = (IsAuthenticated,)
-
-#     def get(self, request):
-#         content = {'message': 'Hello, World!'}
-#         return Response(content)
