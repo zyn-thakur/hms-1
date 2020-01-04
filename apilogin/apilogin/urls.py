be89b824565fbcd4f314bb46e5348084526d07d2
Lucoa Adams <lucoadam@gmail.com>
2020-01-04 08:41:11 -0800

    urls

--- apilogin/apilogin/urls.py
+++ apilogin/apilogin/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path,include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from django.contrib import admin
 from . import api
